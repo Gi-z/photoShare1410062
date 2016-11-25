48ea7b12405ad1bce5a33414cf39121d36f8fea2
Ubuntu <ubuntu@ip-172-31-32-53.us-west-2.compute.internal>
2016-11-25 14:50:25 +0000

    Registration is now working. API outputs an unsuccessful response, but it's working.

---
+++ app/models/post.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema({
+	title: String,
+	user: String,
+	uploaded_at: Date,
+	location: String,
+	meta: {
+		caption: String,
+		exif: {
+			creation_date: Date,
+			dimensions: String,
+			exposure_time: Number,
+			jpeg_quality: String,
+			aperture: String,
+			color_mode: String,
+			flash_used: Boolean,
+			focal_length: Number,
+			iso: Number,
+			jpeg_process: String,
+			camera_manufacturer: String,
+			metering_mode: String,
+			camera_model: String,
+			orientation: Number
+		},
+		geotag_name: String,
+		geotag_location_long: Number,
+		geotag_location_lat: Number
+	},
+	comments: String
+});
+
+var Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
