fc2de38f8e6eab6e4cefa2a053ac32434341b4bf
Ubuntu <ubuntu@ip-172-31-32-53.us-west-2.compute.internal>
2016-12-05 02:17:42 +0000

    Saving progress. Continuing on finalising small parts of functionality and design principles.

--- public/js/controllers/backupDiscoverController.js
+++ public/js/controllers/backupDiscoverController.js
@@ -1,9 +1,9 @@
 var app = angular.module("discoverApp");
 
 app.controller("discoverController", ["$scope", "discoverService", function ($scope, discoverService) {
+	
 	var promise = discoverService.getPosts();
 	promise.then(function(postResp) {
-		$scope.error = false;
 		$scope.posts = JSON.parse(postResp["data"]["posts"]);
 	}, function(error) {
 		$scope.error = true;
